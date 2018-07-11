23c01817085941da840d390bbdb54be37409da45
MegaKreja <kreja95ue@gmail.com>
2018-07-11 19:29:07 +0200

    responsive for home finished

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from "react-router-dom";
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// EXACT u buducnosti jos ruta kad se napravi komponenta za jedan film i glumce
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App} exact/>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
 registerServiceWorker();
