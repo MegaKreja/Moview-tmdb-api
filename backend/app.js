eeea78b7144c66280c83c657156fe06897abb42e
MegaKreja <kreja95ue@gmail.com>
2019-07-30 21:49:59 +0200

    backend boilerplate

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const uuidv4 = require('uuid/v4');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(result => {
+    app.listen(process.env.PORT || 8000);
+    console.log('connected');
+  })
+  .catch(err => console.log(err));
