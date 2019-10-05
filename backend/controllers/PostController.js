531a09aaf75ba8f41682784703b95b7a7edfc306
MegaKreja <kreja95ue@gmail.com>
2019-10-05 20:55:52 +0200

    finished addReview controller

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -1,3 +1,53 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const Review = require('../models/review');
+
 exports.addReview = (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
+  const { user, movie, review } = req.body;
+  Review.findOne({ tmdbId: movie.id })
+    .then(foundedReview => {
+      if (!foundedReview) {
+        const newReview = new Review({
+          tmdbId: movie.id,
+          reviews: [
+            {
+              userId: user._id,
+              username: user.username,
+              text: review,
+              likes: 0
+            }
+          ]
+        });
+        newReview.save().then(result => {
+          User.findOne({ username: user.username }).then(user => {
+            user.reviews.push(result._id);
+            user.save().then(user => {
+              res.status(201).json({ message: 'Added post' });
+            });
+          });
+        });
+      } else {
+        foundedReview.reviews.push({
+          userId: user._id,
+          username: user.username,
+          text: review,
+          likes: 0
+        });
+        foundedReview.save().then(result => {
+          User.findOne({ username: user.username }).then(user => {
+            user.reviews.push(result._id);
+            user.save().then(user => {
+              res.status(201).json({ message: 'Added post' });
+            });
+          });
+        });
+      }
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
