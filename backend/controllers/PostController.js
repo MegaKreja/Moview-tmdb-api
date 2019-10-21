0a71a308f3af66b7fafcab0e9a587250819f2877
MegaKreja <kreja95ue@gmail.com>
2019-10-21 10:12:18 +0200

    completed edit review controller

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -62,11 +62,11 @@ exports.addReview = (req, res, next) => {
 exports.getReviews = (req, res, next) => {
   const tmdbId = Number(req.params.id);
   Review.findOne({ tmdbId })
-    .then(review => {
-      if (!review) {
+    .then(movie => {
+      if (!movie) {
         res.status(200).json({ reviews: [] });
       } else {
-        res.status(200).json({ reviews: review.reviews });
+        res.status(200).json({ reviews: movie.reviews });
       }
     })
     .catch(err => {
@@ -78,5 +78,20 @@ exports.getReviews = (req, res, next) => {
 };
 
 exports.editReview = (req, res, next) => {
-  console.log(req);
+  console.log(req.body);
+  const { editedReview, tmdbId, index } = req.body;
+  Review.findOne({ tmdbId: Number(tmdbId) })
+    .then(movie => {
+      console.log(movie);
+      movie.reviews[index].text = editedReview;
+      movie.save().then(savedReview => {
+        res.status(200).json({ message: 'Updated successfully' });
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
